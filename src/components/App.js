60b6f91f9e90b4baabdb6e75c510d9d1445e3717
kunle babatunde <kb2232@nyu.edu>
2019-11-22 04:43:19 -0500

    project structure added

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <div className="component">
+        START HERE GO ANYWHERE!!!
+      </div>
+    )
+  }
+
+}
+export default App;
